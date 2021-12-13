776ba1394bf142952b9a734b9baffcaf222f2464
unknown <dgarita@mail.com>
2021-12-13 00:07:42 -0600

    Login con usuario creado funcional

---
+++ app/src/main/java/com/isc/topmovies/SingInActivity.kt
@@ -0,0 +1,71 @@
+package com.isc.topmovies
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.util.Log
+import android.widget.Toast
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.ktx.auth
+import com.google.firebase.ktx.Firebase
+import com.isc.topmovies.databinding.ActivityMainBinding
+import com.isc.topmovies.databinding.ActivitySingInBinding
+
+class SingInActivity : AppCompatActivity() {
+    private lateinit var auth: FirebaseAuth
+    private lateinit var binding: ActivitySingInBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivitySingInBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        // Initialize Firebase Auth
+        auth = Firebase.auth
+
+        binding.signInAppCompatButton.setOnClickListener{
+            val mEmail = binding.emailEditText.text.toString()
+            val mPassword = binding.passwordEditText.text.toString()
+
+            when{
+                mEmail.isEmpty() || mPassword.isEmpty() ->{
+                    Toast.makeText(baseContext, "Correo o contraseña no validos",
+                        Toast.LENGTH_SHORT).show()
+                }else ->{
+                    SignIn(mEmail, mPassword)
+                }
+            }
+
+
+        }
+    }
+    //Para que una vez se logue un usuario no tenga que volver a hacerlo
+    public override fun onStart() {
+        super.onStart()
+        // Check if user is signed in (non-null) and update UI accordingly.
+        val currentUser = auth.currentUser
+        if(currentUser != null){
+            reload();
+        }
+    }
+
+    private fun SignIn(email : String, password : String){
+        auth.signInWithEmailAndPassword(email, password)
+            .addOnCompleteListener(this) { task ->
+                if (task.isSuccessful) {
+                    Log.d("TAG", "signInWithEmail:success")
+                    reload()
+                } else {
+                    Log.w("TAG", "signInWithEmail:failure", task.exception)
+                    Toast.makeText(baseContext, "Correo o contraseña no validos",
+                        Toast.LENGTH_SHORT).show()
+                }
+            }
+    }
+
+
+    private fun reload(){
+        val intent = Intent(this, MainActivity::class.java)
+        this.startActivity(intent)
+    }
+
+}
